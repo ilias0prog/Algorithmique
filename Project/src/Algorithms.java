3f5f89086770bf677f30b89ddef456749f0a76be
ilias0prog <123807317+ilias0prog@users.noreply.github.com>
2024-05-02 20:08:52 +0200

    The glouton will work
    I promise

---
+++ Project/src/Algorithms.java
@@ -0,0 +1,373 @@
+import java.util.*;
+import java.lang.Math;
+import java.lang.System;
+
+public interface Algorithms {
+    Random rng = new Random();
+
+    // Position initiale du héros
+    int[] heroPos = {4, 0};
+
+    // Santé initiale du héros
+    int heroHealth = 100;
+
+    // Score initial du héros
+    int heroScore = 0;
+
+    // Tableau représentant les monstres dans le niveau
+    int[][] monsters = new int[][] {
+            {0, 20, 0, 0, 0, 15, 0},
+            {0, 0, 10, 0, 5, 10, 0},
+            {0, 30, 0, 0, 0, 0, 10},
+            {0, 0, 5, 0, 10, 0, 20},
+            {0, 10, 0, 0, 0, 10, 0},
+            {0, 0, 0 , 15, 0, 0, 0},
+            {5 , 0, 0, 0, 30, 0, 0},
+            {0, 0, 5, 0, 10, 0, 20},
+            {0, 10, 0, 0, 0, 10, 0},
+            {0, 0, 0 , 15, 0, 0, 0},
+            {5 , 0, 0, 0, 30, 0, 0}
+    };
+
+    // Tableau représentant les trésors dans le niveau
+    int[][] treasures = new int[][] {
+            {0, 10, 0, 0, 0, 0, 0},
+            {0, 0, 0, 20, 0, 0, 0},
+            {0, 0, 0, 15, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 20},
+            {0, 10, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 5 , 0},
+            {0, 30, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 20},
+            {0, 10, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 5 , 0},
+            {0, 30, 0, 0, 0, 0, 0}
+    };
+
+    // Nombre initial de conseils disponibles pour le joueur
+    int nbHint = 1;
+
+    // Numéro du niveau actuel
+    int nbLevel = 1;
+
+    // Initialisation de l'objet State
+    State initialState = new State(heroPos, heroHealth, heroScore, monsters, treasures, nbHint, nbLevel);
+
+    int score_to_beat = GS.greedySolution(initialState);
+
+    static void main(String[] args) {
+        // Your code here
+        System.out.println("Hello world!");
+    }
+
+    /*** --- Generate & Test --- ***/
+    interface GT {
+        static void generateMonstersAndTreasures(int[][] monstersToFill, int[][] treasuresToFill) {
+            // TODO
+        }
+
+        /*** --- Utility functions for GT --- ***/
+        // TODO (if you have any)
+    }
+
+    /*** --- Divide & Conquer --- ***/
+    interface DC {
+        static void sortLevel(int[][] monstersToSort, int[][] treasuresToSort) {
+            // TODO
+        }
+
+        /*** --- Utility functions for DC --- ***/
+        // TODO (if you have any)
+    }
+
+    /*** --- Greedy Search --- ***/
+
+    private static int getValueAtCoords(int[][] board, int x, int y) {
+        return board[x][y];
+    }
+
+    private static int getPathScore(State state, String path) {
+        int pathLength = path.length();
+        int score = state.heroScore + state.heroHealth;
+        int heroX = state.heroPos[0];
+        int heroY = state.heroPos[1];
+        int height = state.monsters.length;
+        int width = state.monsters[0].length;
+
+        for (int i = 0; i < pathLength; i++) {
+            char direction = path.charAt(i);
+            switch (direction) {
+                case 'd':
+                    if (heroY < height) {
+                        // Faire descendre le héros
+                        heroY += 1;
+                        break;
+                    }
+                case 'r':
+                    if (heroX < width) {
+                        // Faire avancer le héros
+                        heroX += 1;
+                        break;
+                    }
+                case 'l':
+                    if (width < heroX) {
+                        heroX -= 1;
+                        break;
+                    }
+            }
+            // Ramasser le trésor s'il y'en a un
+            score += getValueAtCoords(state.treasures, heroX, heroY);
+            // Combattre le monstre s'il y'en a un
+            score -= getValueAtCoords(state.monsters, heroX, heroY);
+        }
+        return score;
+    }
+
+    /**
+     *
+     * Spécification : Ilias Essarhiri
+     * Implémentation: Ilias Essarhiri + ajustement de la dernière ligne par ChatGPT
+     * pour ajuster la dernière ligne.
+     *
+     * Representation : startsquare and targetSquare represent coordinates of this
+     * form : {1,2} if x coordinate is 1 and y coordinate is 2;
+     *
+     * @requires : startSquare and targetSquare contain 2 positive integers;
+     * @return : true if targetSquare can be reached in n moves from startSquare,
+     *         false otherwise;
+     */
+
+    // @requires startSquare != null && targetSquare != null;
+    // @return = ( (startY == targetY && deltaX == n) || (deltaX + deltaY) <= n &&
+    // (deltaX + deltaY) % 2 == 1) )
+    // @ensures \result = ?
+    // @ Pure
+    private static boolean isReachableInNMoves(int[] startSquare, int[] targetSquare, int n) {
+        int startX = startSquare[0];
+        int startY = startSquare[1];
+        int targetX = targetSquare[0];
+        int targetY = targetSquare[1];
+
+        // Check if target square upper than start square
+        if (targetY < startY)
+            return false;
+
+        int deltaX = Math.abs(targetX - startX);
+        int deltaY = Math.abs(targetY - startY);
+
+        // True if both squares are on the same line and n squares far from eachother
+        if (startY == targetY && deltaX == n)
+            return true;
+
+        // The intuition behind this line was given by chatGPT
+        return (deltaX + deltaY) <= n && (deltaX + deltaY) % 2 == 1;
+    }
+
+    private static ArrayList<int[]> getAllValidPathsNSquaresFrom(int[] startSquare, int n) {
+        ArrayList<int[]> allSquares = new ArrayList<int[]>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int[] newSquare = { i, j };
+                if (isReachableInNMoves(startSquare, newSquare, n))
+                    allSquares.add(newSquare);
+            }
+        }
+        return allSquares;
+    }
+
+    private static StringBuilder getPathTo(int[] startSquare, int[] targetSquare, State state) {
+        StringBuilder path = new StringBuilder();
+        int startX = startSquare[0];
+        int startY = startSquare[1];
+        int targetX = targetSquare[0];
+        int targetY = targetSquare[1];
+        int height = state.monsters.length;
+        int width = state.monsters[0].length;
+
+        // Déplacer horizontalement vers la droite ou la gauche jusqu'à atteindre la
+        // même colonne que la cible
+        while (startX != targetX) {
+            if (startX < targetX && startX < width) {
+                path.append('r');
+                startX++;
+            }
+            if (0 < startX) {
+                path.append('l');
+                startX--;
+            }
+        }
+
+        while (startY < targetY && startY < height) {
+            path.append('d');
+            startY++;
+        }
+        return path;
+    }
+
+    private static String getBestDirection(int[] startSquare, State state) {
+        ArrayList<int[]> allSquares = new ArrayList<int[]>();
+        allSquares = getAllValidPathsNSquaresFrom(startSquare, 5);
+        int bestScore = -999999999;
+        StringBuilder bestDir = new StringBuilder();
+        for (int[] allSquare : allSquares) {
+            StringBuilder path = new StringBuilder();
+            path = getPathTo(startSquare, allSquare, state);
+            int score = getPathScore(state, path.toString());
+            if (score > bestScore) {
+                bestDir = path;
+                bestScore = score;
+            }
+        }
+        return bestDir.substring(0, 1);
+    }
+
+//    private static void moveHero(State state, int[] startSquare, String path) {
+//        int pathLength = path.length();
+//        for (int i = 0; i < pathLength; i++) {
+//            char direction = path.charAt(i);
+//            int heroX = startSquare[0];
+//            int heroY = startSquare[1];
+//            int height = state.monsters.length;
+//            int width = state.monsters[0].length;
+//
+//            switch (direction) {
+//                case 'd':
+//                    if (heroY < height) {
+//                        // Faire descendre le héros
+//                        state.heroPos[1] += 1;
+//                    }
+//                case 'r':
+//                    if (heroX < width) {
+//                        // Faire avancer le héros
+//                        state.heroPos[0] += 1;
+//                        //
+//                    }
+//                case 'l':
+//                    if (width < heroX) {
+//                        state.heroPos[0] -= 1;
+//                    }
+//            }
+//            // Ramasser le trésor s'il y'en a un
+//            state.heroScore += getValueAtCoords(state.treasures, heroX, state.heroPos[1]);
+//            // Combattre le monstre s'il y'en a un
+//            state.heroHealth -= getValueAtCoords(state.monsters, heroX, state.heroPos[1]);
+//            if (state.heroHealth < 1) {
+//                System.out.println("T'es mort");
+//            }
+//        }
+//    }
+
+    private static void moveHero(State state, String path) {
+        int pathLength = path.length();
+        int height = state.monsters.length;
+        int width = state.monsters[0].length;
+
+        for (char direction : path.toCharArray()) {
+            switch (direction) {
+                case 'd':
+                    if (state.heroPos[1] < height) {
+                        // Faire descendre le héros
+                        state.heroPos[1] += 1;
+                    }
+                    break;
+                case 'r':
+                    if (state.heroPos[0] < width) {
+                        // Faire avancer le héros
+                        state.heroPos[0] += 1;
+                    }
+                    break;
+                case 'l':
+                    if (width < state.heroPos[0]) {
+                        state.heroPos[0] -= 1;
+                    }
+                    break;
+            }
+            // Ramasser le trésor s'il y'en a un
+            state.heroScore += getValueAtCoords(state.treasures, state.heroPos[0], state.heroPos[1]);
+            // Combattre le monstre s'il y'en a un
+            state.heroHealth -= getValueAtCoords(state.monsters, state.heroPos[0], state.heroPos[1]);
+            if (state.heroHealth < 1) {
+                System.out.println("T'es mort");
+            }
+            System.out.println("Hero position : " + Arrays.toString(state.heroPos));
+            System.out.println("Hero score : " + state.heroScore);
+        }
+    }
+
+//    private static boolean heroCanMove(State state){
+//        if(state.heroHealth > 1) return false;
+//
+//        // Check surrounding squares
+//        int leftMonster = state.monsters[state.heroPos[0] - 1][state.heroPos[1]];
+//        int rightMonster = state.monsters[state.heroPos[0] + 1][state.heroPos[1]];
+//        int bottomMonster = state.monsters[state.heroPos[0]][state.heroPos[1] + 1];
+//
+//        int leftTreasure = state.treasures[state.heroPos[0] - 1][state.heroPos[1]];
+//        int rightTreasure = state.treasures[state.heroPos[0] + 1][state.heroPos[1]];
+//        int bottomTreasure = state.treasures[state.heroPos[0]][state.heroPos[1] + 1];
+//
+//        int minMonster = Math.min(leftMonster,rightMonster);
+//        minMonster = Math.min(minMonster, bottomMonster);
+//        int maxTreasure = Math.max(leftTreasure,rightTreasure);
+//        maxTreasure = Math.max(maxTreasure, bottomTreasure);
+//
+//        return state.heroHealth >= (state.heroHealth + maxTreasure - minMonster);
+//    }
+
+    private static boolean heroCanMove(State state) {
+        int heroX = state.heroPos[0];
+        int heroY = state.heroPos[1];
+        int height = state.monsters.length;
+        int width = state.monsters[0].length;
+
+        if (heroX < 0 || heroX >= width || heroY < 0 || heroY >= height) {
+            // Le héros est en dehors des limites du niveau
+            return false;
+        }
+
+        int monster = state.monsters[heroX][heroY];
+        int treasure = state.treasures[heroX][heroY];
+
+        // Condition de retour basée sur la logique du jeu
+        return state.heroHealth >= (state.heroHealth + treasure - monster);
+    }
+
+
+    interface GS {
+        static int greedySolution(State state) {
+            int[] startSquare = state.heroPos;
+            int height = state.monsters.length;
+            int width = state.monsters[0].length;
+            int score_to_beat = 0;
+
+            while (state.heroPos[0] < width && state.heroPos[1] < height) {
+                System.out.println("################################");
+                String best_direction = getBestDirection(startSquare, state);
+                System.out.println(best_direction);
+                int path_score = getPathScore(state, best_direction);
+                moveHero(state, best_direction);
+
+                score_to_beat += path_score;
+            }
+            System.out.println("Score to beat : " + score_to_beat);
+            return score_to_beat;
+        }
+        /*** --- Utility functions for GS --- ***/
+        // TODO (if you have any)
+    }
+
+    /*** --- Dynamic Programming --- ***/
+    interface DP {
+        static String perfectSolution(State state) {
+            // TODO
+            return "";
+        }
+
+        /*** --- Utility functions for DP --- ***/
+        // TODO (if you have any)
+    }
+
+    /* --- Common utility functions --- */
+    // TODO (if you have any)
+
+}
--- Project/src/Algorithms2.java
+++ Project/src/Algorithms2.java
@@ -1,40 +1,36 @@
 import java.util.*;
 
-
-public interface Algorithms {
+public interface Algorithms2 {
     Random rng = new Random();
-    int height = 11;
-    int width = 7;
 
     static void main(String[] args) {
         // Your code here
         System.out.println("Hello world!");
     }
 
-
     /*** --- Generate & Test --- ***/
     interface GT {
         static void generateMonstersAndTreasures(int[][] monstersToFill, int[][] treasuresToFill) {
-            //TODO
+            // TODO
         }
 
         /*** --- Utility functions for GT --- ***/
-        //TODO (if you have any)
+        // TODO (if you have any)
     }
 
     /*** --- Divide & Conquer --- ***/
     interface DC {
         static void sortLevel(int[][] monstersToSort, int[][] treasuresToSort) {
-            //TODO
+            // TODO
         }
 
         /*** --- Utility functions for DC --- ***/
-        //TODO (if you have any)
+        // TODO (if you have any)
     }
 
     /*** --- Greedy Search --- ***/
 
-    private int getValueAtCoords(int[][] board, int x, int y){
+    private int getValueAtCoords(int[][] board, int x, int y) {
         for (int[] square : board) {
             if (square[0] == x && square[1] == y) {
                 return square[2];
@@ -42,9 +38,13 @@ public interface Algorithms {
         }
         return 0;
     }
-    private void moveHero(State state, String path){
+
+    private void moveHero(State state, String path) {
+        int height = state.monsters.length;
+        int width = state.monsters[0].length;
+
         int pathLength = path.length();
-        for(int i = 0; i < pathLength; i++){
+        for (int i = 0; i < pathLength; i++) {
             char direction = path.charAt(i);
             int heroX = state.heroPos[0];
             int heroY = state.heroPos[1];
@@ -53,34 +53,39 @@ public interface Algorithms {
                     if (heroY < height) {
                         // Faire descendre le héros
                         state.heroPos[1] += 1;
+                        break;
                     }
                 case 'r':
                     if (heroX < width) {
                         // Faire avancer le héros
                         state.heroPos[0] += 1;
-                        //
+                        break;
                     }
                 case 'l':
                     if (width < heroX) {
                         state.heroPos[0] -= 1;
+                        break;
                     }
             }
             // Ramasser le trésor s'il y'en a un
             state.heroScore += getValueAtCoords(state.treasures, heroX, state.heroPos[1]);
             // Combattre le monstre s'il y'en a un
             state.heroHealth -= getValueAtCoords(state.monsters, heroX, state.heroPos[1]);
-//                if(state.heroHealth < 1){
-//                    // Dead
-//                }
+            // if(state.heroHealth < 1){
+            // // Dead
+            // }
         }
     }
 
-    private int getPathScore(State state, String path){
+    private int getPathScore(State state, String path) {
         int pathLength = path.length();
         int score = state.heroScore;
         int heroX = state.heroPos[0];
         int heroY = state.heroPos[1];
-        for(int i = 0; i < pathLength; i++){
+        int height = state.monsters.length;
+        int width = state.monsters[0].length;
+
+        for (int i = 0; i < pathLength; i++) {
             char direction = path.charAt(i);
             switch (direction) {
                 case 'd':
@@ -107,46 +112,52 @@ public interface Algorithms {
         return score;
     }
 
-    private char bestDirection(State state){
+    private char bestDirection(State state) {
         char bestDir;
-        int  bestScore;
+        int bestScore;
         int heroX = state.heroPos[0];
         int heroY = state.heroPos[1];
         // try left :
-        if(0 < state.heroPos[0]){
-            int[] coords = {heroX-1, heroY};
+        if (0 < state.heroPos[0]) {
+            int[] coords = { heroX - 1, heroY };
             ArrayList<int[]> leftOptions = new ArrayList<>();
             leftOptions = getAllSquaresReachableWithNMoves(coords, 4);
-            for(int[] square : leftOptions){
+            for (int[] square : leftOptions) {
 
             }
         }
-        return bestDir;
+        return 'a';
     }
 
     /**
      * Spécification : Ilias Essarhiri
-     * Implémentation: Ilias Essarhiri + ajustement de la dernière ligne par ChatGPT pour s'assurer que la méthode est fiable dans tous les cas de figure
+     * Implémentation: Ilias Essarhiri + ajustement de la dernière ligne par ChatGPT
+     * pour s'assurer que la méthode est fiable dans tous les cas de figure
      *
-     * Representation : startsquare and targetSquare represent coordinates of this form : {1,2} if x coordinate is 1 and y coordinate is 2;
+     * Representation : startsquare and targetSquare represent coordinates of this
+     * form : {1,2} if x coordinate is 1 and y coordinate is 2;
+     *
      * @requires : startSquare and targetSquare contain 2 positive integers;
-     * @return : true if targetSquare can be reached in 5 moves from startSquare, false otherwise;
-     * **/
+     * @return : true if targetSquare can be reached in 5 moves from startSquare,
+     *         false otherwise;
+     **/
 
-    //@requires startSquare != null && targetSquare != null;
-    //@ensures /return = ...
+    // @requires startSquare != null && targetSquare != null;
+    // @ensures /return = ...
     private boolean isReachableInFiveMoves(int[] startSquare, int[] targetSquare) {
         int startX = startSquare[0];
         int startY = startSquare[1];
         int targetX = targetSquare[0];
         int targetY = targetSquare[1];
 
-        if (targetY < startY) return false;
+        if (targetY < startY)
+            return false;
 
         int deltaX = Math.abs(targetX - startX);
         int deltaY = Math.abs(targetY - startY);
 
-        if (startY == targetY && deltaX == 5) return true;
+        if (startY == targetY && deltaX == 5)
+            return true;
 
         return (deltaX + deltaY) <= 5 && (deltaX + deltaY) % 2 == 1;
     }
@@ -158,66 +169,69 @@ public interface Algorithms {
         int targetY = targetSquare[1];
 
         // Vérifier si la case cible est au-dessus de la case de départ
-        if (targetY < startY) return false;
+        if (targetY < startY)
+            return false;
 
         // Calculer la distance horizontale et verticale entre les deux cases
         int deltaX = Math.abs(targetX - startX);
         int deltaY = Math.abs(targetY - startY);
 
-        // Vérifier si la case cible est sur le même étage que la case de départ et à une distance de dist déplacements horizontaux
-        if (startY == targetY && deltaX == n) return true;
+        // Vérifier si la case cible est sur le même étage que la case de départ et à
+        // une distance de dist déplacements horizontaux
+        if (startY == targetY && deltaX == n)
+            return true;
 
-        // Vérifier si la case cible est accessible en utilisant au plus dist déplacements alternés entre horizontal et vertical
+        // Vérifier si la case cible est accessible en utilisant au plus dist
+        // déplacements alternés entre horizontal et vertical
         return (deltaX + deltaY) <= n && (deltaX + deltaY) % 2 == 1;
     }
 
-
-    private ArrayList<int[]> getAllSquaresReachableWithNMoves(int[] startSquare, int n){
+    private ArrayList<int[]> getAllSquaresReachableWithNMoves(int[] startSquare, int n) {
         ArrayList<int[]> allSquares = new ArrayList<>();
-        for(int i = 0; i<n ; i++){
-            for(int j = 0; j<n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 int squareX = i + startSquare[0];
                 int squareY = j + startSquare[1];
-                int[] newSquare = {squareX,squareY};
-                if(isReachableInNMoves(startSquare, newSquare, 5)) allSquares.add(newSquare);
+                int[] newSquare = { squareX, squareY };
+                if (isReachableInNMoves(startSquare, newSquare, 5))
+                    allSquares.add(newSquare);
             }
         }
         return allSquares;
     }
 
-
-
-    private String getShortestPath(int[] startSquare, int[] targetSquare){
+    private String getShortestPath(int[] startSquare, int[] targetSquare) {
         String s = "aaa";
         return s;
     }
 
-    private String bestPathToThisSquare(State state, int[] startSquare, int[] targetSquare){
+    private String bestPathToThisSquare(State state, int[] startSquare, int[] targetSquare) {
         String s = "aaa";
         return s;
     }
+
     interface GS {
         static int greedySolution(State state) {
-            //TODO
+            // TODO
             return 0;
         }
 
         /*** --- Utility functions for GS --- ***/
-        //TODO (if you have any)
+        // TODO (if you have any)
     }
 
     /*** --- Dynamic Programming --- ***/
     interface DP {
         static String perfectSolution(State state) {
-            //TODO
+            // TODO
             return "";
         }
 
         /*** --- Utility functions for DP --- ***/
-        //TODO (if you have any)
+        // TODO (if you have any)
     }
 
     /* --- Common utility functions --- */
-    //TODO (if you have any)
+    // TODO (if you have any)
 
 }
